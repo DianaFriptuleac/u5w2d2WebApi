61fd0f98733ac3dfb031d6a9f98f54f601b81057
Diana <dianadorojuc3@gmail.com>
2024-10-29 13:22:21 +0100

    Initial commit

---
+++ src/main/java/dianafriptuleac/u5w2d2WebApi/U5w2d2WebApiApplication.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w2d2WebApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2d2WebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2d2WebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianafriptuleac/u5w2d2WebApi/U5w2d2WebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w2d2WebApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2d2WebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
