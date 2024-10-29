395691b4fe583ff9a34509899a69acc5a2c2be06
Diana <dianadorojuc3@gmail.com>
2024-10-29 15:01:11 +0100

    up autore

--- src/main/java/dianafriptuleac/u5w2d2WebApi/controllers/AutoreController.java
+++ src/main/java/dianafriptuleac/u5w2d2WebApi/controllers/AutoreController.java
@@ -1,4 +1,32 @@
 package dianafriptuleac.u5w2d2WebApi.controllers;
 
+import dianafriptuleac.u5w2d2WebApi.entities.Autore;
+import dianafriptuleac.u5w2d2WebApi.payloads.NewAutorePayload;
+import dianafriptuleac.u5w2d2WebApi.services.AutoreService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/autori")
 public class AutoreController {
+    @Autowired
+    private AutoreService autoreService;
+
+    //1.Get http://localhost:3002/autori
+
+    @GetMapping
+    public List<Autore> getAutore() {
+        return this.autoreService.findAllAutori();
+    }
+
+    //2. Post http://localhost:3002/autori = payload
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Autore createAutore(@RequestBody NewAutorePayload body) {
+        return this.autoreService.saveAutore(body);
+    }
 }
--- src/main/java/dianafriptuleac/u5w2d2WebApi/services/AutoreService.java
+++ src/main/java/dianafriptuleac/u5w2d2WebApi/services/AutoreService.java
@@ -1,4 +1,28 @@
 package dianafriptuleac.u5w2d2WebApi.services;
 
+import dianafriptuleac.u5w2d2WebApi.entities.Autore;
+import dianafriptuleac.u5w2d2WebApi.payloads.NewAutorePayload;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class AutoreService {
+    private List<Autore> autoriList = new ArrayList<>();
+
+    //Cerco la lista di autori
+    public List<Autore> findAllAutori() {
+        return this.autoriList;
+    }
+
+    public Autore saveAutore(NewAutorePayload body) {
+        Random rdm = new Random();
+        Autore newAutore = new Autore(body.getNome(), body.getCognome(), body.getEmail(), body.getDataDiNascita());
+        newAutore.setId(rdm.nextInt(1, 500000));
+        newAutore.setAvatar("newAvatar");
+        return newAutore;
+    }
+
 }
